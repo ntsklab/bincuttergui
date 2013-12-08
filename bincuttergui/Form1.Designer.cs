c83b206553e0551b2943dc264590c7e5cff2be8c
Nit Tokiwa <nit.ipod@gmail.com>
2013-12-08 18:29:01 +0900

    デザイン変更

--- bincuttergui/Form1.Designer.cs
+++ bincuttergui/Form1.Designer.cs
@@ -28,104 +28,104 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.txtStart = new System.Windows.Forms.TextBox();
-			this.txtSize = new System.Windows.Forms.TextBox();
-			this.btnOpen = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.btnMinus100h = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// txtStart
-			// 
-			this.txtStart.Location = new System.Drawing.Point(35, 12);
-			this.txtStart.Name = "txtStart";
-			this.txtStart.Size = new System.Drawing.Size(100, 19);
-			this.txtStart.TabIndex = 0;
-			// 
-			// txtSize
-			// 
-			this.txtSize.Location = new System.Drawing.Point(172, 12);
-			this.txtSize.Name = "txtSize";
-			this.txtSize.Size = new System.Drawing.Size(100, 19);
-			this.txtSize.TabIndex = 1;
-			// 
-			// btnOpen
-			// 
-			this.btnOpen.Location = new System.Drawing.Point(12, 227);
-			this.btnOpen.Name = "btnOpen";
-			this.btnOpen.Size = new System.Drawing.Size(75, 23);
-			this.btnOpen.TabIndex = 2;
-			this.btnOpen.Text = "Run";
-			this.btnOpen.UseVisualStyleBackColor = true;
-			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 15);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(17, 12);
-			this.label1.TabIndex = 4;
-			this.label1.Text = "0x";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(149, 15);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(17, 12);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "0x";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(35, 38);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(72, 12);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "StartAddress";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(172, 38);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(72, 12);
-			this.label4.TabIndex = 7;
-			this.label4.Text = "Size(*1024B)";
-			// 
-			// btnMinus100h
-			// 
-			this.btnMinus100h.Location = new System.Drawing.Point(35, 53);
-			this.btnMinus100h.Name = "btnMinus100h";
-			this.btnMinus100h.Size = new System.Drawing.Size(75, 23);
-			this.btnMinus100h.TabIndex = 8;
-			this.btnMinus100h.Text = "-100h";
-			this.btnMinus100h.UseVisualStyleBackColor = true;
-			this.btnMinus100h.Click += new System.EventHandler(this.btnMinus100h_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Controls.Add(this.btnMinus100h);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.btnOpen);
-			this.Controls.Add(this.txtSize);
-			this.Controls.Add(this.txtStart);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMinus100h = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(35, 12);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 19);
+            this.txtStart.TabIndex = 0;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(172, 12);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 19);
+            this.txtSize.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(197, 227);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Run";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "StartAddress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Size(*1024B)";
+            // 
+            // btnMinus100h
+            // 
+            this.btnMinus100h.Location = new System.Drawing.Point(35, 53);
+            this.btnMinus100h.Name = "btnMinus100h";
+            this.btnMinus100h.Size = new System.Drawing.Size(75, 23);
+            this.btnMinus100h.TabIndex = 8;
+            this.btnMinus100h.Text = "-100h";
+            this.btnMinus100h.UseVisualStyleBackColor = true;
+            this.btnMinus100h.Click += new System.EventHandler(this.btnMinus100h_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnMinus100h);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.txtStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 

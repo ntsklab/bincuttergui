c06410ba378d0e226fcd4630b04e6b92606c8dbe
Nit Tokiwa <nit.ipod@gmail.com>
2013-12-08 18:27:13 +0900

    first commit

---
+++ bincuttergui/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace bincuttergui
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtStart = new System.Windows.Forms.TextBox();
+			this.txtSize = new System.Windows.Forms.TextBox();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnMinus100h = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtStart
+			// 
+			this.txtStart.Location = new System.Drawing.Point(35, 12);
+			this.txtStart.Name = "txtStart";
+			this.txtStart.Size = new System.Drawing.Size(100, 19);
+			this.txtStart.TabIndex = 0;
+			// 
+			// txtSize
+			// 
+			this.txtSize.Location = new System.Drawing.Point(172, 12);
+			this.txtSize.Name = "txtSize";
+			this.txtSize.Size = new System.Drawing.Size(100, 19);
+			this.txtSize.TabIndex = 1;
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(12, 227);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(75, 23);
+			this.btnOpen.TabIndex = 2;
+			this.btnOpen.Text = "Run";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(17, 12);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "0x";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(149, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(17, 12);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "0x";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(35, 38);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(72, 12);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "StartAddress";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(172, 38);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(72, 12);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Size(*1024B)";
+			// 
+			// btnMinus100h
+			// 
+			this.btnMinus100h.Location = new System.Drawing.Point(35, 53);
+			this.btnMinus100h.Name = "btnMinus100h";
+			this.btnMinus100h.Size = new System.Drawing.Size(75, 23);
+			this.btnMinus100h.TabIndex = 8;
+			this.btnMinus100h.Text = "-100h";
+			this.btnMinus100h.UseVisualStyleBackColor = true;
+			this.btnMinus100h.Click += new System.EventHandler(this.btnMinus100h_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.btnMinus100h);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnOpen);
+			this.Controls.Add(this.txtSize);
+			this.Controls.Add(this.txtStart);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtStart;
+		private System.Windows.Forms.TextBox txtSize;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnMinus100h;
+	}
+}
+
---
+++ bincuttergui/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bincuttergui
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		const int bufsize = 1024;
+		byte[] buf = new byte[bufsize];
+		byte[] zero_array = new byte[bufsize];
+		int readed_size = 0;
+		int total_readed_size = 0;
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		void btnOpen_Click(object sender, EventArgs e)
+		{
+			if (txtStart.Text.Length == 0 | txtSize.Text.Length == 0)
+				MessageBox.Show("Error!");
+			else
+			{
+				OpenFileDialog ofd = new OpenFileDialog();
+				ofd.FileName = "";
+				ofd.InitialDirectory = "";
+				ofd.Title = "Open File Name";
+				ofd.RestoreDirectory = true;
+				if (ofd.ShowDialog() == DialogResult.OK)
+				{
+					FileStream ofs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+
+					SaveFileDialog sfd = new SaveFileDialog();
+					sfd.Title = "Save File Name";
+					sfd.FileName = "";
+					sfd.InitialDirectory = "";
+					sfd.RestoreDirectory = true;
+					if (sfd.ShowDialog() == DialogResult.OK)
+					{
+						FileStream sfs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+
+						ofs.Seek(Convert.ToInt32(txtStart.Text, 16), SeekOrigin.Begin);
+
+						while (true)
+						{
+							try
+							{
+								readed_size = ofs.Read(buf, 0, bufsize);
+								total_readed_size += readed_size;
+							}
+							catch
+							{
+								MessageBox.Show("Read Err at " + ofs.Position);
+
+								if (ofs.Length - ofs.Position < bufsize)
+								{
+									readed_size = (int)(ofs.Length - ofs.Position);
+								}
+								else
+								{
+									readed_size = bufsize;
+								}
+								ofs.Seek(readed_size, SeekOrigin.Current);
+								sfs.Write(zero_array, 0, readed_size);
+								continue;
+							}
+							if (total_readed_size > Convert.ToInt32(txtSize.Text, 16) | readed_size == 0)
+								break;
+							sfs.Write(buf, 0, readed_size);
+						}
+						ofs.Close();
+						sfs.Close();
+						readed_size = 0;
+						total_readed_size = 0;
+						MessageBox.Show("Done!");
+					}
+				}
+			}
+		}
+
+		private void btnMinus100h_Click(object sender, EventArgs e)
+		{
+			if (txtStart.Text.Length != 0)
+			{
+				int tmp = Convert.ToInt32(txtStart.Text, 16) - 0x100;
+				txtStart.Text = Convert.ToString(tmp, 16);
+			}
+		}
+	}
+}
